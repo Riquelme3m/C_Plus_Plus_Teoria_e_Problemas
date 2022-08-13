f57d92fdd17a88eb46aa830955ab5107495062bc
Riquelme Batist <riquelme3m@outlook.com>
2022-08-12 21:56:11 -0300

    5.4

---
+++ Funções/5.4.cpp
@@ -0,0 +1,18 @@
+/*Programa que verifica a relação fundamental da trigonometria cos^2x + sen^2x = 1*/
+#include<cmath>
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    float x;
+    cout<<"Digite um número: "<<endl;
+    cin>>x;
+    cout<<pow(cos(x),2)+pow(sin(x),2)<<" = "<<"1 "<<endl;
+
+
+
+
+
+    return 0;
+}
