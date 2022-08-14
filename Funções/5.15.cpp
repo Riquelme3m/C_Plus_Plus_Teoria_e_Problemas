9a040c8d6a2a76cb6e732c11c42f2cb79795522f
Riquelme Batista <riquelme3m@outlook.com>
2022-08-14 18:48:43 -0300

    5.12 and 5.15

--- Funções/5.15.cpp
+++ Funções/5.15.cpp
@@ -2,22 +2,22 @@
 #include<iostream>
 using namespace std;
 
-int perm(int n,int k);
+int combina(int n,int k);
 int main(){
     
-    for(int i=-1;i<6;i++){
+    for(int i=-1;i<9;i++){
         for(int j=-1;j<=i+1;j++)
-        cout<<" "<<perm(i,j);
+        cout<<" "<<combina(i,j);
         cout<<endl;
         }
 
 return 0;
 }
 
-int perm(int n,int k){
+int combina(int n,int k){
     int c=1;
     for(int i=1;i<=k;i++,n--)
-    c*=n;
+    c=c*n/i;
 return c;
     
 }
