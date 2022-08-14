b7cc1ac14208ed985671b6f4b0fb7938eaa98078
Riquelme Batista <riquelme3m@outlook.com>
2022-08-14 18:35:30 -0300

    5.26

--- Funções/5.26.cpp
+++ Funções/5.26.cpp
@@ -4,15 +4,22 @@
 
 using namespace std;
 
-void computeTriangle(float& a,float&p,float x,float y,float z);
+void computeSphere(float& s,float&v,float r);
  
 int main(){
-    cout<<"Digite os comprimentos do lado desse triângulo na seguinte ordem (cateto adjacente,cateto oposto,hipotenusa): "<<endl;
-    float area,perimetro,a,b,c;
-    cin>>a>>b>>c;
-    computeTriangle(area,perimetro,a,b,c);
-    cout<<"A área desse triângulo é : " <<area<<endl;
-    cout<<"O perímetro desse triângulo é : "<<perimetro<<endl;
+    cout<<"Digite o raio da esfera: "<<endl;
+    float area,volume,raio;
+    cin>>raio;
+    computeSphere(area,volume,raio);
+    cout<<"O volume dessa esfera é : "<<area<<endl;
+    cout<<"A area dessa esfera é : "<<volume<<endl;
 
     return 0;
+}
+
+void computeSphere(float& s,float&v,float r){
+    double const PI=3.141598;
+    s=4*PI*pow(r,2);
+    v=4*PI*pow(r,3)/3;
+
 }
