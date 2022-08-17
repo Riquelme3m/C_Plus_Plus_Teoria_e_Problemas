795712e007baa3f81146c738926bb8925be999c3
Riquelme Batista <riquelme3m@outlook.com>
2022-08-17 20:56:33 -0300

    algoritmo bublle sort

--- Arrays/array.cpp
+++ Arrays/array.cpp
@@ -1,26 +1,27 @@
 #include<iostream>
 using namespace std;
 
+void sort(float[],int);
+
 int main(){
+  
+float a[]={55.5,22.5,99.9,66.6,44.4,88.8,33.3,77.7};
+sort(a,8);
 
-int matriz[3][4];
-
-
-for(int i=0;i<3;i++){
-    for(int j=0;j<4;j++){
-        matriz[i][j]=i;
-    }
-}
-
-for(int i=0;i<3;i++){
-    for(int j=0;j<4;j++){
-        cout<<matriz[i][j]<<"   ";
-    }
-    cout<<endl;
-}
 
 
 
 
     return 0;
+}
+
+void sort(float a[],int n){
+    for(int i=1;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            if(a[j]>a[j+1]) swap(a[j],a[j+1]);
+        }
+    }
+   for(int i=0;i<n;i++){
+       cout<<a[i]<<endl;
+   }
 }
