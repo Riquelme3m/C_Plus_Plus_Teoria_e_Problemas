e7e3a6780b6381a22fa66547c3990c4cee3e43d6
Riquelme Batista <riquelme3m@outlook.com>
2022-08-20 13:31:46 -0300

    6.27

---
+++ Arrays/6.27.cpp
@@ -0,0 +1,30 @@
+//Implementação de uma função que soma os dois valores de um vetor com o mesmo número de elementos n;
+#include<iostream>
+
+using namespace std;
+
+void produto(float [],int,float []);
+
+
+int main(){
+    float vetora[]={0,1,2,3,4};
+    float vetorb[]={0,1,2,3,4};
+    produto(vetora,5,vetorb);
+
+
+    return 0;
+}
+void produto(float a[],int n,float b[]){
+float acumulador=0.0;
+for(int i=0;i<n;i++){
+    acumulador=a[i]*b[i]+acumulador;
+}
+
+/*for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+}
+*/
+cout<<"O produto interno desses dois vetores é : "<<acumulador<<endl;
+
+
+}
