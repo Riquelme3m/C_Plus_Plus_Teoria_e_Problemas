6cf4afe8c7e48ae7cd1be818188716d6057db252
Riquelme Batista <riquelme3m@outlook.com>
2022-08-20 13:23:27 -0300

    6.25

---
+++ Arrays/6.25.cpp
@@ -0,0 +1,30 @@
+//Implementação de uma função que soma os dois valores de um vetor com o mesmo número de elementos n;
+#include<iostream>
+
+using namespace std;
+
+void adicionar(float [],int,float []);
+
+
+int main(){
+    float vetora[]={0,1,2,3,4};
+    float vetorb[]={0,1,2,3,4};
+    adicionar(vetora,5,vetorb);
+
+
+    return 0;
+}
+void adicionar(float a[],int n,float b[]){
+
+for(int i=0;i<n;i++){
+    a[i]+=b[i];
+}
+
+for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+}
+
+
+
+
+}
